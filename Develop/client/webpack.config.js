cfa04210c10cf2c141335568cb8c3a7316b69791
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-24 11:36:39 -0800

    feat: add Webpack loaders for CSS and JavaScript
    - Configured `style-loader` and `css-loader` to handle CSS files
    - Added `babel-loader` for transpiling JavaScript with `@babel/preset-env`
    - Excluded `node_modules` from Babel processing to optimize build performance

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -48,7 +48,20 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
