c89a27fb753fa7bec1bcbadf696a1200c6e41392
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-24 13:13:26 -0800

    PACKAGES

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -11,8 +11,9 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
+    
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
