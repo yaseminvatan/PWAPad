2ae90dd952a436ea953896146025cae7e6f1cd0d
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-24 10:34:51 -0800

    feat: configure Webpack for PWA support and HTML template integration
    - Added HtmlWebpackPlugin to generate the HTML file from a template
    - Integrated InjectManifest to inject service worker from `src-sw.js`
    - Configured WebpackPwaManifest for PWA setup with app details, icons, and theme colors

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,7 +18,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor PWA!',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        fingerprints: false,
+        inject: true,
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
